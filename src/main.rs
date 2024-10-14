27e002ba17b2981385461f67b77933dcafe27d30
Jan-Eric Nitschke <47750513+JanEricNitschke@users.noreply.github.com>
2024-10-14 18:20:58 +0200

    Fix direct errors

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
-use clap::{App, Arg};
+use clap::{Command, Arg, ArgAction};
 
 use std::process;
 
 fn main() {
-    let matches = App::new("rchef")
+    let matches = Command::new("rchef")
         .version("0.1")
         .author("boolean_coercion <booleancoercion@gmail.com>")
         .about("A fully-featured interpreter for the esoteric programming language Chef.")
         .arg(
             Arg::new("spaced")
-                .about("Determines whether your program will run in spaced mode.")
+                .help("Determines whether your program will run in spaced mode.")
                 .short('s')
-                .long("spaced"),
+                .long("spaced")
+                .action(ArgAction::SetTrue) // Sets to true if flag is present
+                .default_value("false"),    // Default is false if flag is absent
         )
         .arg(
             Arg::new("filename")
-                .about("The filename of the program you intend to run.")
+                .help("The filename of the program you intend to run.")
                 .required(true),
         )
         .get_matches();
 
-    let filename = matches.value_of("filename").unwrap(); // filename is required
-    let spaced = matches.is_present("spaced");
+        let filename = matches.get_one::<String>("filename").unwrap(); // filename is required
+        let spaced = matches.get_flag("spaced");
 
     if let Err(why) = rchef::run(filename, spaced) {
         eprintln!("{}", why);
